49a2829060acbe0b9b3d6143eef71532cb503705
semir <semir@trendcare.com.au>
2020-05-28 14:01:27 +0100

    Add Getter/Setter methods.

--- app/src/main/java/edu/uc/jonesbr/myplantdiary/ui/main/MainViewModel.kt
+++ app/src/main/java/edu/uc/jonesbr/myplantdiary/ui/main/MainViewModel.kt
@@ -6,14 +6,22 @@ import edu.uc.jonesbr.myplantdiary.dto.Plant
 import edu.uc.jonesbr.myplantdiary.service.PlantService
 
 class MainViewModel : ViewModel() {
-    var plants: MutableLiveData<ArrayList<Plant>> = MutableLiveData<ArrayList<Plant>>()
-    var plantService: PlantService =  PlantService()
+    private var _plants: MutableLiveData<ArrayList<Plant>> = MutableLiveData<ArrayList<Plant>>()
+    var plantService: PlantService = PlantService()
 
     init {
         fetchPlants("e")
     }
 
     fun fetchPlants(plantName: String) {
-        plants = plantService.fetchPlants(plantName)
+        _plants = plantService.fetchPlants(plantName)
     }
+
+    internal var plants: MutableLiveData<ArrayList<Plant>>
+        get() {
+            return _plants
+        }
+        set(value) {
+            _plants = value
+        }
 }
